d068f3be95c47e3949a9b33ebe62af0c82f8a514
JianShuiHuang <47372304+JianShuiHuang@users.noreply.github.com>
2020-09-06 22:14:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import random
 import matplotlib.pyplot as plt
 from dataloader import *
@@ -8,10 +9,35 @@ import time
 from Time import *
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+def show1(a1, Epochs):
+    plt.title('Accuracy Curve', fontsize = 18)
+    plt.xlabel('Epochs')
+    
+    x = []
+    for i in range(Epochs):
+        x.append(i)
+        plt.plot(i, a1[i], 'bo')
+    
+    plt.plot(x, a1)
+    plt.show()
+    
+def show2(a, b, Epochs):
+    plt.title('generator and discriminator loss', fontsize = 18)
+    x = []
+    for i in range(Epochs):
+        x.append(i)
+        plt.plot(i, a[i], 'bo')
+        plt.plot(i, b[i], 'ro')
+    
+    plt.plot(x, a, b)
+    plt.show()
+    
+
+
 image_size = 64
-lr_G = 0.0002
+lr_G = 0.0001
 lr_D = 0.0001
-batch_size = 128
+batch_size = 64
 num_epochs = 1000
 input_size_g = 124
 hidden_size_g = 64
@@ -20,6 +46,7 @@ input_size_d = 4
 hidden_size_d = 64
 output_size_d = 1
 latent_size = 100
+
 
 manualSeed = 999
 random.seed(manualSeed)
@@ -39,3 +66,26 @@ accuracy = training(generator, discriminator, image_size, latent_size, lr_G, lr_
 print("Time: ", timeSince(start, 1 / 100))
 
 print("Max Accuracy: ", max(accuracy))
+
+
+
+g_loss = np.load("Gloss.npy")
+d_loss = np.load("Dloss.npy")
+accuracy = np.load("Accuracy.npy")
+
+
+show1(accuracy, 1200)
+
+show2(g_loss, d_loss, 1200)
+
+
+"""
+generator = torch.load('generator.pkl', map_location = torch.device('cpu'))
+acc, imgs = testing(generator)
+print("Accuracy: ", acc)
+
+fig = plt.figure(figsize=(15,15))
+plt.imshow(np.transpose(imgs[0],(1,2,0)))
+"""
+
+
