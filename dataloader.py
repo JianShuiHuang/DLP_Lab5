ba1cb5bfd0106f528cec5649fe1f1b164ac56d46
JianShuiHuang <47372304+JianShuiHuang@users.noreply.github.com>
2020-09-06 22:12:53 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -6,11 +6,15 @@ from torch.utils import data
 import torchvision.transforms as transforms
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 def get_data(mode):
     data = json.load(open('./lab5_dataset/'+mode+'.json', 'r'))
     if mode == 'train':
-        data = [i for i in data.items()]
-    return np.squeeze(data)
+        Data = []
+        for item in data.items():
+            Data.append(item)
+
+    return np.squeeze(Data)
 
 def dictionary():
     return json.load(open('./lab5_dataset/objects.json', 'r'))
@@ -19,13 +23,7 @@ class DataLoader(data.Dataset):
     def __init__(self, mode, image_size=64):
         self.mode = mode   
         self.data = get_data(mode)
-        self.obj_dict = dictionary()
-        self.transformation = transforms.Compose([
-                                  transforms.Resize(image_size),
-                                  transforms.CenterCrop(image_size),
-                                  transforms.ToTensor(),
-                                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                              ])
+        self.dict = dictionary()
         
     def __len__(self):
         return len(self.data)
@@ -35,10 +33,10 @@ class DataLoader(data.Dataset):
             img_name = self.data[index][0]
             objects = []
             for item in self.data[index][1]:
-                objects.append(self.obj_dict[item])
+                objects.append(self.dict[item])
 
-            img = np.array(Image.open('./lab5_dataset/iclevr/'+img_name))[...,:-1]
-            img = self.transformation(Image.fromarray(img))
+            img = np.array(Image.open('./lab5_dataset/iclevr/'+img_name))
+            img = np.asarray(img)/255
             
             condition = torch.zeros(24)
             condition = torch.tensor([v+1 if i in objects else v for i,v in enumerate(condition)])
@@ -47,7 +45,7 @@ class DataLoader(data.Dataset):
         else:
             objects = []
             for item in self.data[index]:
-                objects.append(self.obj_dict[item])
+                objects.append(self.dict[item])
             condition = torch.zeros(24)
             data = torch.tensor([v+1 if i in objects else v for i,v in enumerate(condition)])
         
