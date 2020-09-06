65240580cf27ca7b5718fb4592295506ce01a2ae
JianShuiHuang <47372304+JianShuiHuang@users.noreply.github.com>
2020-09-06 22:42:18 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -39,11 +39,12 @@ class DataLoader(data.Dataset):
                 objects.append(self.dict[item])
 
             img = np.array(Image.open('./lab5_dataset/iclevr/'+img_name))[...,:-1]
+            img = img / 255
             img = Image.fromarray(img)
             img = transforms.Resize(self.image_size)
             img = transforms.CenterCrop(self.image_size)
             img = transforms.ToTensor()
-            img = img / 255
+            
             
             
             condition = torch.zeros(24)
